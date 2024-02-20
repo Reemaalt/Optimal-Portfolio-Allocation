f4e268dbf1bfb33bd70d17a34eb2b1140472d9ff
Reemaalt <reema.altj@gmail.com>
2024-02-20 23:04:23 +0300

    fixed some error, start working ,add empty asset class

--- BruteForce.java
+++ BruteForce.java
@@ -1,23 +1,32 @@
+
+import java.util.List;
+
 public class BruteForce  {
  
+/*Loop through all possible asset allocations using nested loops.
+ a. Calculate the total return and total risk for the current allocation.
+b. If the risk is within the specified tolerance and return is greater than maxReturn:
+i. Update maxReturn, minRisk, and optimalAllocation. */
+
+public void assetAlocater (String ID , double MAXreturn ,double minRisk ,List<asset> optimalInvsment ){
+
+}
  
- 
-public static void runprogram()
+public void runprogram()
     {
 // Read input from the text file نورة 
         readInput("Example.txt");
-
-        // Call the brute force method to find optimal allocation ريما
+// Call the brute force method to find optimal allocation ريما
         bruteForce(0, 0, new ArrayList<>());
-
-        // Write optimal allocation and results to output file نوف
+// Write optimal allocation and results to output file نوف
         writeOutput("Output_BruteForce.txt");
 
     }
 
 // Main method to run the program
     public static void main(String[] args) {
-        runprogram();
+        BruteForce BruteForce = new BruteForce();
+        BruteForce.runprogram();
         System.out.println("done!");
     }
 
